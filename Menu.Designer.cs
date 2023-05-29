b1672c375e2b842196ce8a31c7f4d96b17694010
AdamLadnak <a.ladnak25@gmail.com>
2023-05-29 21:09:50 +0200

    register

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -42,59 +42,67 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 244);
+            this.button1.Location = new System.Drawing.Point(207, 198);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 54);
+            this.button1.Size = new System.Drawing.Size(88, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "Prihlásiť sa";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(264, 155);
+            this.textBox1.Location = new System.Drawing.Point(198, 126);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(264, 199);
+            this.textBox2.Location = new System.Drawing.Point(198, 162);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(163, 22);
+            this.textBox2.Size = new System.Drawing.Size(123, 20);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(261, 136);
+            this.label1.Location = new System.Drawing.Point(196, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.Size = new System.Drawing.Size(104, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Použivateľské meno";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(261, 180);
+            this.label2.Location = new System.Drawing.Point(196, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.Size = new System.Drawing.Size(34, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Heslo";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(283, 347);
+            this.label3.Location = new System.Drawing.Point(212, 282);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 16);
+            this.label3.Size = new System.Drawing.Size(102, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Chcem len prezerať ";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(286, 366);
+            this.button2.Location = new System.Drawing.Point(214, 297);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 42);
+            this.button2.Size = new System.Drawing.Size(70, 34);
             this.button2.TabIndex = 6;
             this.button2.Text = "Prezerať";
             this.button2.UseVisualStyleBackColor = true;
@@ -106,24 +114,26 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 113);
+            this.panel1.Size = new System.Drawing.Size(600, 92);
             this.panel1.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(303, 48);
+            this.label4.Location = new System.Drawing.Point(227, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(134, 16);
+            this.label4.Size = new System.Drawing.Size(110, 13);
             this.label4.TabIndex = 0;
             this.label4.Text = "Inventarizácia SPŠ IT";
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
@@ -132,6 +142,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu";
             this.Text = "Menu";
             this.panel1.ResumeLayout(false);
--- Menu.cs
+++ Menu.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,47 @@ namespace Invetarizácia
         {
             Form1 form = new Form1();
             form.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT meno, heslo FROM login WHERE meno = @Meno AND heslo = @Heslo";
+            MySqlConnection con = Dbitem.GetConnection();
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+
+            string zadaneMeno = textBox1.Text;
+            string zadaneHeslo = textBox2.Text;
+
+            cmd.Parameters.AddWithValue("@Meno", zadaneMeno);
+            cmd.Parameters.AddWithValue("@Heslo", zadaneHeslo);
+
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string menoVDB = reader.GetString(reader.GetOrdinal("meno"));
+
+                        if (zadaneMeno == menoVDB)
+                        {
+                            Console.WriteLine("Meno sa zhoduje!");
+                            Register form = new Register();
+                            form.ShowDialog();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Meno sa nezhoduje!");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Meno nebolo nájdené v databáze!");
+                }
+            }
+
+            con.Close();
         }
     }
 }
